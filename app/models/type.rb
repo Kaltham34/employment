1c5cbf742e8471eb8c85c7c139ad8f0de2eb5d6f
Kaltham34 <k_alkhuzaei@hotmail.com>
2016-04-18 20:29:14 +0300

    Update type.rb

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,4 +1,4 @@
 class Type < ActiveRecord::Base
-	has_many :jobs
-	validates :name,presence: true, uniqueness:{ case_sensitive: false}
+   has_many :jobs
+   validates :name, presence: true, uniqueness: true
 end
