7e4d68709dd69d39aa0afd582592fdfa1045651d
Kaltham34 <k_alkhuzaei@hotmail.com>
2016-04-18 20:25:38 +0300

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,31 @@
 class SessionsController < ApplicationController
-	skip_before_action :authorize
+	skip_before_action :authenticate
 	def new
 	end
-
 	def create
-		user=User.find_by(email: params[:sessions][:email].downcase)
-		# if user && user.password == params[:demo][:password]
-		if user && user.authenticate(params[:sessions][:password])
-			log_in user
-			redirect_to user
-			#login user
-		else
-			flash.now[:danger] = "invalid email/password"
-			render 'new'
-		end
-	end
+      user = User.find_by(email:
+          params[:sessions][:email].downcase)
+      if user && user.password ==
+          params[:sessions][:password]
+        log_in user
+        flash[:notice] = 'Logged in'
+        redirect_to root_path
+      else
+        flash.now[:alert] = 'Invalid email/password'
+        render 'new'
+      end
+  end
 
-	def destroy
-		logout
-		redirect_to root_path
+    def destroy
+        log_out
+        flash[:notice] = 'Logged out'
+        redirect_to root_path
+    end
 
-	end
+	  
+
+
+
 end
 
 
